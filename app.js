12bf2bb2164c5a35b41f385eabb42afbccf33ad6
VirLg <19virlg@gmail.com>
2024-01-10 17:06:15 +0200

    add start files

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import logger from 'morgan';
+import dotenv from 'dotenv';
+
+// import carsRouter from './routes/cars.js';
+// import authRouter from './routes/users.js';
+dotenv.config();
+const app = express();
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+app.use(cors());
+app.use(logger(formatsLogger));
+app.use(express.json());
+
+// app.use('/api/cars', carsRouter);
+// app.use('/api/users', authRouter);
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = 'Error Server' } = err;
+  res.status(status).json({ message });
+});
+
+export default app;
